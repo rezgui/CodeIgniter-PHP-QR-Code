e563ae436f6fae346d2c1da411608c97d292fc65
Yacine REZGUI <yacine.rezgui@gmail.com>
2016-11-03 11:28:06 +0100

    update project

---
+++ config/ciqrcode.php
@@ -0,0 +1,9 @@
+$config['cacheable']	= true; //boolean, the default is true
+$config['cachedir']		= ''; //string, the default is application/cache/
+$config['errorlog']		= ''; //string, the default is application/logs/
+$config['quality']		= true; //boolean, the default is true
+$config['size']			= ''; //interger, the default is 1024
+$config['black']		= array(224,255,255); // array, default is array(255,255,255)
+$config['white']		= array(70,130,180); // array, default is array(0,0,0)
+
+
